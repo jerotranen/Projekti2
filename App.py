739b59eba92aa576d5b0e9739ce5e5627314ac9a
jerotranen <jerotranen@gmail.com>
2024-04-04 19:55:09 +0300

    update2

--- App.py
+++ App.py
@@ -1,27 +1,34 @@
 import sys
+import sqlite3
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QStringListModel
+from PyQt5.QtCore import QStringListModel, QDate, Qt
 from mainwin import Ui_MainWindow 
 
 class MyApp(QMainWindow):
     def __init__(self):
-
-        # setup
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.calendarWidget.selectionChanged.connect(self.handle_date_selection)
+        self.task_model = QStringListModel()
+        self.ui.listView.setModel(self.task_model)
 
-        # yhdistää handleriin
-        self.ui.pushButton.clicked.connect(self.handle_button_click)
-        self.list_model = QStringListModel()
-        self.ui.listView.setModel(self.list_model)
+    def handle_date_selection(self):
+        selected_date = self.ui.calendarWidget.selectedDate()
+        print(selected_date)
+        year = selected_date.year()
+        month = selected_date.month()
+        day = selected_date.day()
+        tasks = ["task1", "task2"]
+        dateobj = Date(year, month, day, tasks)
+        self.task_model.setStringList(dateobj.tasks)
 
-    def handle_button_click(self):
-        text = self.ui.lineEdit.text()
-        string_list = self.list_model.stringList()
-        string_list.append(text)
-        self.list_model.setStringList(string_list)
-        print(text)
+class Date:
+    def __init__(self, year, month, day, tasks):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.tasks = tasks
 
 def main():
     app = QApplication(sys.argv)
