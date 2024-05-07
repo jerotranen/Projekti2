04e26786649649928d8795b5f7313adc274d9a8c
jerotranen <jerotranen@gmail.com>
2024-05-07 15:37:25 +0300

    final2

---
+++ Notification.py
@@ -0,0 +1,39 @@
+import time
+import sched
+from plyer import notification
+
+def main(tasksToRemind):
+
+    # Metodi notifikaatioille
+    def remindMe(task_desc):
+        notification.notify(
+            title="Task Reminder",
+            message=task_desc,
+            timeout=6,
+            app_name="MyApp",
+            app_icon=None,
+            ticker="Task Reminder",
+            toast=False
+        )
+
+    # Lista johonka tallennetaan kaikki päivän muistutukset threadin startattua
+    reminders = []
+    # Alustetaan scheduler
+    s = sched.scheduler(time.time, time.sleep)
+    current_time = time.time()
+    current_hour = time.localtime(current_time).tm_hour
+    current_minute = time.localtime(current_time).tm_min
+
+    # Muunnetaan parametrina saatu lista käytettävämpään muotoon -> reminders-listaan
+    for task in tasksToRemind:
+        hour, minute, task_desc = task.split(':')
+        task_hour, task_minute = int(hour), int(minute)
+        reminder_time = current_time + ((task_hour - current_hour) * 3600) + ((task_minute - current_minute - 1) * 60)
+        reminders.append((reminder_time, task_desc))
+
+    # Jokainen reminder saa oman schedulerinsa
+    for reminder in reminders:
+        reminder_time, task_desc = reminder
+        s.enterabs(reminder_time, 1, remindMe, argument=(task_desc,))
+    # Runnaa schedulerit
+    s.run()
